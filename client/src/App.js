4bf7d2cbf976816c3717f8174b72e31652e61f27
yebinge <yebing@student.unimelb.edu.au>
2022-12-01 21:15:14 +1100

    Delete styles.css

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,6 @@ import React from "react";
 
 import Header from "./components/Header";
 import Profiles from "./components/Profiles";
-import Container from "@mui/material/Container";
-
-import "./styles.css";
 
 export default function App() {
   return (
