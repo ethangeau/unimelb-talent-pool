86b01470666020eebb0caa34f71322108caae5cf
yebinge <yebing@student.unimelb.edu.au>
2022-12-02 17:20:16 +1100

    Config router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Profiles from "./components/Profiles";
+import Auth from "./components/Auth/Auth";
+import NotFound from "./components/NotFound";
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Profiles />
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Profiles />} />
+          <Route path="auth" element={<Auth />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </>
   );
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,33 +1,68 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 import { AppBar, Typography } from "@mui/material";
 import SchoolIcon from "@mui/icons-material/School";
 
 export default function Header() {
   return (
-    <AppBar
-      position="static"
-      sx={{
-        bgcolor: "#023D80",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        borderRadius: 5,
-        height: "5rem",
-      }}
-    >
-      <div
-        style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+    <>
+      <AppBar
+        position="static"
+        sx={{
+          bgcolor: "#023D80",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          borderRadius: 5,
+          height: "5rem",
+        }}
       >
-        <SchoolIcon sx={{ fontSize: 40, color: "#fff", marginLeft: "1rem" }} />
-        <Typography variant="h5" sx={{ ml: "5px" }}>
-          Unimelb Talent Pool
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <SchoolIcon
+            sx={{ fontSize: 40, color: "#fff", marginLeft: "1rem" }}
+          />
+          <Typography
+            component={Link}
+            to="/"
+            variant="h5"
+            sx={{
+              ml: "1rem",
+              color: "#fff",
+              textDecoration: "none",
+              "&:hover": {
+                textDecoration: "underline",
+              },
+            }}
+          >
+            Unimelb Talent Pool
+          </Typography>
+        </div>
+        <Typography variant="h5">Search</Typography>
+
+        <Typography
+          component={Link}
+          to="/auth"
+          variant="h5"
+          sx={{
+            mr: "2rem",
+            color: "#fff",
+            textDecoration: "none",
+            "&:hover": {
+              textDecoration: "underline",
+            },
+          }}
+        >
+          Sign in/up
         </Typography>
-      </div>
-      <Typography variant="h5">Search</Typography>
-      <Typography variant="h5" sx={{ mr: "2rem" }}>
-        Sign in/up
-      </Typography>
-    </AppBar>
+      </AppBar>
+      <Outlet />
+    </>
   );
 }
