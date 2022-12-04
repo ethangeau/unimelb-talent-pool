1cdcf78842aa8e8399456af3cd5f4060b5cffdc9
yebinge <yebing@student.unimelb.edu.au>
2022-12-05 09:11:18 +1100

    Config redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+import { Provider } from "react-redux";
+import { configureStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
+
+const store = configureStore(reducers, compose(applyMiddleware(thunk)));
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
