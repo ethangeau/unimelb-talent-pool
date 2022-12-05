0be2ca32e978edf9870342b31c1972f8bddb7ecf
yebinge <yebing@student.unimelb.edu.au>
2022-12-05 15:38:36 +1100

    Add user info submit function

--- client/src/components/UserInfo.jsx
+++ client/src/components/UserInfo.jsx
@@ -1,26 +1,50 @@
 import React, { useState } from "react";
 import { Avatar, Button, Paper, Grid, Container } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { createProfile, updateProfile } from "../actions/profiles";
 
 import FileBase from "react-file-base64";
 import Input from "./Auth/Input";
 
+const initialProfileData = {
+  avatar: "",
+  firstName: "",
+  lastName: "",
+  role: "",
+  introduction: "",
+  email: "",
+  personalSite: "",
+  linkedin: "",
+  github: "",
+  instagram: "",
+};
+
 export default function UserInfo() {
+  const userProfile = null;
   const hasInfo = false;
-  const [formState, setFormState] = useState({
-    firstName: "",
-    lastName: "",
-    role: "",
-    introduction: "",
-    email: "",
-    personalSite: "",
-    linkedin: "",
-    github: "",
-    instagram: "",
-  });
-  const handleSubmit = () => {};
-  const handleChange = (e) => {
-    setFormState({ ...formState, [e.target.name]: e.target.value });
+  const [formData, setFormData] = useState(
+    userProfile ? userProfile : initialProfileData
+  );
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (userProfile) {
+      dispatch(updateProfile(userProfile._id, formData));
+    } else {
+      dispatch(createProfile(formData));
+    }
+    // Can't figure out why this is not working
+    clearFormData();
   };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const clearFormData = () => {
+    setFormData(initialProfileData);
+  };
+
   return (
     <Container maxWidth="md">
       <Paper
@@ -54,7 +78,13 @@ export default function UserInfo() {
               >
                 A
               </Avatar>
-              <FileBase type="file" multiple={false} onDone={() => {}} />
+              <FileBase
+                type="file"
+                multiple={false}
+                onDone={({ base64 }) =>
+                  setFormData({ ...formData, avatar: base64 })
+                }
+              />
             </div>
             <Input
               name="firstName"
