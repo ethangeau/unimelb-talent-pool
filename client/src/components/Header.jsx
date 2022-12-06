2192d9899debd7f15430dd9802c53e5a3b6cf3b6
yebinge <yebing@student.unimelb.edu.au>
2022-12-06 22:59:40 +1100

    Add authentication in Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,41 @@
-import React from "react";
-import { Link, Outlet } from "react-router-dom";
-import { AppBar, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Link, Outlet, useNavigate, useLocation } from "react-router-dom";
+import { AppBar, Avatar, Typography } from "@mui/material";
+import decode from "jwt-decode";
+
 import SchoolIcon from "@mui/icons-material/School";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+
+import * as actionTypes from "../constants/actionTypes";
 
 export default function Header() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const logout = () => {
+    dispatch({ type: actionTypes.LOGOUT });
+
+    navigate("/auth");
+
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const token = user?.token;
+
+    if (token) {
+      const decodedToken = decode(token);
+
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
+
+    setUser(JSON.parse(localStorage.getItem("profile")));
+  }, [location]);
+
   return (
     <>
       <AppBar
@@ -46,34 +77,58 @@ export default function Header() {
           </Typography>
         </div>
         <Typography variant="h5">Search</Typography>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <Link to="/userinfo">
-            <AccountCircleIcon
-              sx={{ fontSize: 40, color: "#fff", mr: "1rem" }}
-            />
-          </Link>
-          <Typography
-            component={Link}
-            to="/auth"
-            variant="h5"
-            sx={{
-              mr: "2rem",
-              color: "#fff",
-              textDecoration: "none",
-              "&:hover": {
-                textDecoration: "underline",
-              },
+        {user?.result ? (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
             }}
           >
-            Sign in/up
-          </Typography>
-        </div>
+            <Link to="/userinfo">
+              <Avatar
+                size="small"
+                src={user.avatar}
+                sx={{ width: "2.5rem", height: "2.5rem" }}
+              >
+                <InsertEmoticonIcon />
+              </Avatar>
+            </Link>
+            <Typography
+              onClick={logout}
+              variant="h5"
+              sx={{
+                ml: "1rem",
+                mr: "2rem",
+                color: "#fff",
+                textDecoration: "none",
+                "&:hover": {
+                  textDecoration: "underline",
+                },
+              }}
+            >
+              Logout
+            </Typography>
+          </div>
+        ) : (
+          <div>
+            <Typography
+              component={Link}
+              to="/auth"
+              variant="h5"
+              sx={{
+                mr: "2rem",
+                color: "#fff",
+                textDecoration: "none",
+                "&:hover": {
+                  textDecoration: "underline",
+                },
+              }}
+            >
+              Sign in/up
+            </Typography>
+          </div>
+        )}
       </AppBar>
       <Outlet />
     </>
