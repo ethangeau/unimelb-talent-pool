6631a462023ea0eaeb92d3a43fabf208a9a5d3a1
yebinge <yebing@student.unimelb.edu.au>
2022-12-10 17:03:00 +1100

    Update notfound page layout

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function NotFound() {
-  return <h1>NotFound</h1>;
+  return (
+    <h1 style={{ fontSize: "3rem", textAlign: "center", marginTop: "10rem" }}>
+      404 Not Found
+    </h1>
+  );
 }
